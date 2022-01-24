62b7a9651722380193f536098c5208efb3e6fba6
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-01-24 15:06:08 -0500

    modify base colors

--- src/screens/CountriesScreen.tsx
+++ src/screens/CountriesScreen.tsx
@@ -1,6 +1,30 @@
+import { BsSearch } from "react-icons/bs";
 
 const CountriesScreen = () => {
-  return <div>Countries Screen</div>;
+  return (
+    <div className="theme countries">
+      <div className="container">
+        <div className="countries__data-entry-container">
+          <div className="countries__input-field">
+              <BsSearch className="countries__icon" />
+              <input placeholder="Search for a country..."></input>
+          </div>
+
+          <div className="countries__select-field">
+            <select>
+              <option value="">Filter by Region</option>
+              <option value="america">América</option>
+              <option value="africa">Africa</option>
+              <option value="europa">Europa</option>
+              <option value="oceania">Oceanía</option>
+            </select>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  )
 };
 
 export default CountriesScreen;
