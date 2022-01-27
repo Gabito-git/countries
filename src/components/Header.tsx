91e29cdac3d9801af1d54feb07a7bfcc070166a6
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-01-27 10:00:44 -0500

    add change theme feature to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
 import { BsMoon } from "react-icons/bs";
+import { ThemeContext } from "../router/AppRouter";
 
 const Header = () => {
+
+  const { themeIsDark, setThemeIsDark } = useContext(ThemeContext);
+
   return (
-    <div className="theme header">
+    <div className={`header ${ themeIsDark ? 'theme--dark': 'theme' }`}>
         <h1 className="header__text">Where in the world?</h1>
-        <div className="header__darkfield">
+        <div 
+          className="header__darkfield"
+          onClick={ () => setThemeIsDark( t => !t ) }
+        >
             <BsMoon />
             <p>Dark mode</p>
         </div>
