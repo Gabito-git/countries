dbee33f3f61300aa603db9da3548b4c8bcda3c3d
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-01-29 15:10:15 -0500

    add get all countries request on load

--- src/screens/CountriesScreen.tsx
+++ src/screens/CountriesScreen.tsx
@@ -1,12 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import CountryList from "../components/CountryList";
 import { ThemeContext } from "../router/AppRouter";
+import { Country } from '../interfaces/countriesInterface';
 
 const CountriesScreen = () => {
 
   const { themeIsDark } = useContext(ThemeContext);
+  const [countries, setCountries] = useState([] as Country[]);
 
+  useEffect(() => {
+    const getAllCountries = async() => {
+      const resp = await fetch('https://restcountries.com/v3.1/all')
+      const data = await resp.json();
+
+      setCountries(data);
+    }
+
+    getAllCountries();
+  }, []);
+  
   return (
     <div className={ `countries ${ themeIsDark ? 'theme--dark': 'theme' }` }>
       <div className="container">
@@ -27,7 +40,7 @@ const CountriesScreen = () => {
           </div>
 
         </div>
-        <CountryList/>
+        <CountryList countries={ countries }/>
       </div>
     </div>
   )
