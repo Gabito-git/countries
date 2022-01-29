f5e1964dafe0296b0acf4154eb9d3b8dd71311a7
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-01-29 15:09:20 -0500

    add dinamyc data to country card

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,8 +1,15 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { Country } from "../interfaces/countriesInterface";
 import { ThemeContext } from '../router/AppRouter';
 
-const CountryCard = () => {
+interface Props{
+  country: Country
+}
+
+const CountryCard = ({ country }: Props) => {
+
+  const { name, population, region, capital, flags } = country;
 
   const { themeIsDark } = useContext(ThemeContext);
   const navigate = useNavigate();
@@ -13,17 +20,17 @@ const CountryCard = () => {
         onClick={() => navigate('./country/12') }
       >
             <img 
-                src="https://flagcdn.com/w320/tz.png"
+                src={ flags.png }
                 alt="flag" 
                 className="countrycard__flag"
             />
 
           <div className="countrycard__country-info">
-            <h2 className="countrycard__country-name">Iceland</h2>
+            <h2 className="countrycard__country-name">{ name.common }</h2>
             <div className="countrycard__country-text">
-                <p>Pupulation: <span>334.200</span></p>
-                <p>Region: <span>Europe</span></p>
-                <p>Capital: <span>Kabul</span></p>
+                <p>Pupulation: <span>{ population }</span></p>
+                <p>Region: <span>{ region }</span></p>
+                <p>Capital: <span>{ capital }</span></p>
             </div>
          </div>  
           
