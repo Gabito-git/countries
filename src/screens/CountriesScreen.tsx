7911f4fabafe5fca457372148b8e07e9cb3e3748
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-01-29 17:26:33 -0500

    add useEffects to countri screen

--- src/screens/CountriesScreen.tsx
+++ src/screens/CountriesScreen.tsx
@@ -8,6 +8,8 @@ const CountriesScreen = () => {
 
   const { themeIsDark } = useContext(ThemeContext);
   const [countries, setCountries] = useState([] as Country[]);
+  const [region, setRegion] = useState('');
+  const [countryName, setCountryName] = useState('');
 
   useEffect(() => {
     const getAllCountries = async() => {
@@ -20,21 +22,77 @@ const CountriesScreen = () => {
     getAllCountries();
   }, []);
   
+
+  useEffect(() => {
+
+    const cache = (cb: ( region: string ) => Promise<Country[]>) => {
+      let responses = {} as {[key: string]: Country[]}
+      return async(region: string) => {
+        const cacheObjStr = localStorage.getItem('cache');
+        responses = JSON.parse(cacheObjStr!) || responses
+        if( responses.hasOwnProperty( region ) ){
+          setCountries(responses[region])
+        }else{
+          responses[region] = await cb( region );
+          localStorage.setItem('cache', JSON.stringify(responses))
+          setCountries(responses[region])
+        }
+      }
+    }
+
+    const getCountriesByRegion = async(region: string): Promise<Country[]> => {
+      const resp = await fetch(`https://restcountries.com/v3.1/region/${region}`)
+      const data = await resp.json()
+      return data;
+    }
+
+    const countriesCache = cache( getCountriesByRegion);
+    region && countriesCache( region )
+
+  }, [region]);
+
+  useEffect(() => {
+    const getCountry = async() => {
+      const resp = await fetch(`https://restcountries.com/v3.1/name/${ countryName }`)
+      const data = await resp.json();
+
+      setCountries(data);
+    }
+
+    countryName && getCountry();
+  }, [countryName]);
+  
+  
+
+  const handleSelectChange = (e : React.ChangeEvent<HTMLSelectElement>) => {
+    setRegion(e.target.value)
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCountryName( e.target.value )
+  }
+  
   return (
     <div className={ `countries ${ themeIsDark ? 'theme--dark': 'theme' }` }>
       <div className="container">
         <div className="countries__data-entry-container">
           <div className="countries__input-field">
               <BsSearch className="countries__icon" />
-              <input placeholder="Search for a country..."></input>
+              <input 
+                placeholder="Search for a country..." 
+                onChange={ handleInputChange }
+                value={ countryName}
+              />
           </div>
 
           <div className="countries__select-field">
-            <select>
+            <select
+              onChange={ handleSelectChange }
+            >
               <option value="">Filter by Region</option>
               <option value="america">América</option>
               <option value="africa">Africa</option>
-              <option value="europa">Europa</option>
+              <option value="europe">Europa</option>
               <option value="oceania">Oceanía</option>
             </select>
           </div>
