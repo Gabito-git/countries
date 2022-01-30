a120418d6fdf33bc928e11836042c8985861b42b
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-01-29 20:33:24 -0500

    implement useCountris hook in countriesscreen component

--- src/screens/CountriesScreen.tsx
+++ src/screens/CountriesScreen.tsx
@@ -4,72 +4,15 @@ import CountryList from "../components/CountryList";
 import { ThemeContext } from "../router/AppRouter";
 import { Country } from '../interfaces/countriesInterface';
 import { fetchHelper } from "../helpers/fetch";
+import { useCountries } from '../hooks/useCountries';
 
 const CountriesScreen = () => {
 
   const { themeIsDark } = useContext(ThemeContext);
-  const [countries, setCountries] = useState([] as Country[]);
-  const [region, setRegion] = useState('');
-  const [countryName, setCountryName] = useState('');
-
-  useEffect(() => {
-    const getAllCountries = async() => {
-      const data = await fetchHelper('all');
-      setCountries(data);
-    }
-
-    getAllCountries();
-  }, []);
   
-
-  useEffect(() => {
-
-    const cache = (cb: ( region: string ) => Promise<Country[]>) => {
-      let responses = {} as {[key: string]: Country[]}
-      return async(region: string) => {
-        const cacheObjStr = localStorage.getItem('cache');
-        responses = JSON.parse(cacheObjStr!) || responses
-        if( responses.hasOwnProperty( region ) ){
-          setCountries(responses[region])
-        }else{
-          responses[region] = await cb( region );
-          localStorage.setItem('cache', JSON.stringify(responses))
-          setCountries(responses[region])
-        }
-      }
-    }
-
-    const getCountriesByRegion = async(region: string): Promise<Country[]> => {
-      return await fetchHelper(`region/${ region }`)
-    }
-
-    const countriesCache = cache( getCountriesByRegion);
-    region && countriesCache( region )
-
-  }, [region]);
-
-  useEffect(() => {
-    const getCountry = async() => {
-      const data = await fetchHelper(`name/${ countryName }`)
-
-      Array.isArray(data) 
-        ? setCountries(data) 
-        : setCountries([])
-    }
-
-    countryName && getCountry();
-  }, [countryName]);
-  
-  
-
-  const handleSelectChange = (e : React.ChangeEvent<HTMLSelectElement>) => {
-    setRegion(e.target.value)
-  }
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCountryName( e.target.value )
-  }
-  
+  const { handleInputChange, handleSelectChange, 
+          countryName, countries} = useCountries()
+ 
   return (
     <div className={ `countries ${ themeIsDark ? 'theme--dark': 'theme' }` }>
       <div className="container">
