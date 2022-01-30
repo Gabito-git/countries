9d1ebb7dd896570f5cb907dba233b3190082ed7e
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-01-29 21:11:08 -0500

    add fetch capability to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,53 @@
 import { Link } from "react-router-dom";
 
 import { BsArrowLeft } from "react-icons/bs";
-
+import { useState, useEffect } from 'react';
+import { fetchHelper } from '../helpers/fetch';
+import { Country } from "../interfaces/countriesInterface";
 
 interface Props{
-    label: string;
+    label?: string;
     hasIcon: boolean;
     where: string;
+    countryId?: string;
 }
 
-const Button = ({label, hasIcon, where}: Props) => {
+const Button = ({label, hasIcon, where, countryId}: Props) => {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [country, setCountry] = useState([] as Country[]);
+
+  useEffect(() => {
+    const getCountryInfo = async() => {
+      const data = await fetchHelper(`alpha/${ countryId }`)
+      setCountry(data);
+    }
+    countryId && getCountryInfo()
+  }, []);
+
+  useEffect(() => {
+    country.length > 0 && setIsLoading(false)
+  }, [country]);
+  
+  const titleButton = !countryId 
+                    ? label
+                    : isLoading && countryId
+                      ? 'Loading'
+                      : country[0].name.common
+
+  const whereTo = !countryId
+                ? where
+                : `/country/${ countryId }`
+  
   return(
       <button className="button">
 
         <Link 
-            to={where}
+            to={ whereTo }
             className="button__link"
         > 
             { hasIcon && <BsArrowLeft className="button__icon"/> }  
-            <span>{ label }</span>
+            <span>{ titleButton }</span>
         </Link>
 
       </button>
