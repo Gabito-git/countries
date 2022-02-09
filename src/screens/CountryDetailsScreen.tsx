06c1ba2601b409fbd968b713d925d1037f718bea
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-02-09 18:37:35 -0500

    last commit

--- src/screens/CountryDetailsScreen.tsx
+++ src/screens/CountryDetailsScreen.tsx
@@ -1,12 +1,49 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useLayoutEffect, useMemo, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Button from "../components/Button";
 import { ThemeContext } from "../router/AppRouter";
+import { Country } from '../interfaces/countriesInterface';
+import { fetchHelper } from '../helpers/fetch';
 
 const CountryDetailsScreen = () => {
 
   const { themeIsDark } = useContext(ThemeContext);
-  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const [country, setCountry] = useState([] as Country[]);
+
+  const { countryId } = useParams();
+
+  useEffect(() => {
+    const getCountryDetails = async() => {
+      const data = await fetchHelper(`alpha/${ countryId }`);
+      setCountry(data);
+    }
+
+    getCountryDetails(); 
+  }, [countryId]);
+
+  useEffect(() => {
+    country.length > 0 && setIsLoading(false)
+  }, [country]);
+  
+
+  if(isLoading) return <h1>Loading</h1>
+
+  const { flags, name, population, 
+          tld, languages, currencies,
+          region, subregion, capital, borders } = country[0]
+
+  const strCurr = Object.values( currencies )
+                        .reduce( (vAnt, vSigu) =>{
+                          return vAnt + vSigu.name + ', '
+                        }, '' )
+
+  const strNativeNames = Object.values( name.nativeName )
+                               .reduce( (vAnt, vSigu) => {
+                                 return vAnt + vSigu.common + ', '
+                               }, '' )
+
+  const formattedPopulation = new Intl.NumberFormat("es-ES").format(population)
 
   return (
     <div className={`detailsscreen ${ themeIsDark ? 'theme--dark': 'theme' }`}>
@@ -17,25 +54,25 @@ const CountryDetailsScreen = () => {
       />
       <div className="detailsscreen__container">
         <div className="detailsscreen__flag">
-          <img src="https://flagcdn.com/tz.svg" alt="flag"/>
+          <img src={ flags.svg } alt="flag"/>
         </div>
         <div className="detailsscreen__text-container">
-          <h2>Belgium</h2>
+          <h2>{ name.common }</h2>
           <div className="detailsscreen__country-data">
             
             <div className="detailsscreen__text-group">
-              <p>Native Name: <span>Belgie</span></p>
-              <p>Population: <span>11.319.511</span></p>
-              <p>Region: <span>Europe</span></p>
-              <p>Sub Region: <span>Western Europe</span></p>
-              <p>Capital: <span>Brussels</span></p>
+              <p>Native Name: <span>{ strNativeNames}</span></p>
+              <p>Population: <span>{ formattedPopulation }</span></p> 
+              <p>Region: <span>{ region }</span></p>
+              <p>Sub Region: <span>{ subregion }</span></p>
+              <p>Capital: <span>{ capital[0] }</span></p>
 
-            </div>
+            </div>  
           
             <div className="detailsscreen__text-group">
-              <p>Top Level Domain: <span>be</span></p>
-              <p>Currencies: <span>Euro</span></p>
-              <p>Languages: <span>Dutch, French, German</span></p>
+              <p>Top Level Domain: <span>{ tld?.join(', ') }</span></p>
+              <p>Currencies: <span>{ strCurr }</span></p>
+              <p>Languages: <span>{Object.values(languages).join(', ')}</span></p>
             </div>
 
           </div>
@@ -43,17 +80,20 @@ const CountryDetailsScreen = () => {
           <div className="detailsscreen__text-group">
             <p>Border Countries:  </p>
             <div className="detailsscreen__borders">
-              <Button label="text" hasIcon={false} where="" />
-              <Button label="text" hasIcon={false} where="" />
-              <Button label="text" hasIcon={false} where="" />
-              <Button label="Netherlands" hasIcon={false} where="" />
+              {
+                borders?.map(border => <Button 
+                  key={border}
+                  hasIcon={false} 
+                  where="" 
+                  countryId={ border } 
+                  />)
+              }
             </div>
           </div>
         </div>
       </div>
       
     </div>
-    
   );
 };
 
